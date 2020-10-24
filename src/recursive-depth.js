9cd56953035505ff00aa15176d606aa682bc6a02
Saylance2503 <yurik_05@inbox.ru>
2020-10-24 17:34:19 +0400

    feat: added solution tower, depth, season

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth ( arr ) {
+    let result = 1;
+    
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray (arr[i])) {
+        let deeper = 1;
+        deeper = 1 + this.calculateDepth (arr[i]);
+        if (deeper > result) {
+          result = deeper;
+        }
+      }
+    }
+    return result;
   }
 };
