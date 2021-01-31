0c2093351b36162b4e9650c51d8fb899fcea42ee
Saylance2503 <yurik_05@inbox.ru>
2021-02-01 00:30:50 +0400

    feat: simple-chain, transform-array

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,21 +1,34 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
+    this.chain.length;
+    return this;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
+    if (value !== undefined) {
+      this.chain.push(`( ${value} )`);
+      return this;
+    }
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
+    if (!position || typeof position !== 'number') {
+      this.chain = [];
+      throw new Error();
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-  }
+    const chainFinish = this.chain.join('~~');
+    this.chain = [];
+    return chainFinish;
+  },
 };
 
 module.exports = chainMaker;
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,5 +1,27 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function transform( arr ) {
-  throw new CustomError('Not implemented');
+module.exports = function transform(arr) {
+  let newArr = [];
+  if (Array.isArray(arr)) {
+    for (let i = 0; i < arr.length; i = i + 1) {
+      if (arr[i] === '--discard-next') {
+        i = i + 1;
+      } else if (arr[i] === '--discard-prev') {
+        if (newArr.length !== 0 && arr[i - 2] !== '--discard-next') {
+          newArr.pop();
+        }
+      } else if (arr[i] === '--double-next') {
+        newArr.push(arr[i + 1]);
+      } else if (arr[i] === '--double-prev') {
+        if (i !== 0 && arr[i - 2] !== '--discard-next') {
+          newArr.push(arr[i - 1]);
+        }
+      } else {
+        newArr.push(arr[i]);
+      }
+    }
+  } else {
+    throw new Error();
+  }
+  return newArr.filter((e) => e !== undefined);
 };
